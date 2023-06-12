5a6e0a7554f11495fcfc44d4cd38509087c0318d
Ruhtra <kawanarthurskate@gmail.com>
2023-06-12 03:14:13 -0300

    added check if any files were deleted from the profiles collection

--- src/config/mongoDB.js
+++ src/config/mongoDB.js
@@ -8,9 +8,30 @@ async function connect() {
  
     const client = new MongoClient(uri);
     await client.connect();
- 
+    
     singleton = client.db('zahir_website');
+    configFunctions()
+    
     return singleton;
+}
+
+function configFunctions() {
+  const profilesCollection = singleton.collection('profile');
+  const carroselCollection = singleton.collection('home_page_promotions');
+
+  const pipeline = [
+    { $match: { operationType: 'delete' } },
+    { $project: { documentKey: true } }
+  ];
+
+  const changeStream = profilesCollection.watch(pipeline);
+  changeStream.on('change', (change) => {
+    const deletedProfileId = change.documentKey._id;
+
+    carroselCollection.deleteOne({ id_profile: deletedProfileId })
+      .then(() => console.log(`${deletedProfileId} removido da home_page_promotions`))
+      .catch((err) => console.error(`Erro ao remover ${deletedProfileId} da home_page_promotion: ${err}`))
+  });
 }
 
 module.exports = { 
