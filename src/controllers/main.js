b5f602519ec0486fbdb9c9e332f80e6fc9f6e279
Ruhtra <kawanarthurskate@gmail.com>
2023-06-18 20:28:19 -0300

    organize controllers main.js

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -1,5 +1,15 @@
 module.exports = {
     index: (req, res) => {
         return res.render('index.ejs')
+    },
+    profile: (req, res) => {
+        return res.render('profile.ejs')
+    },
+    // config
+    profiles: (req, res) => {
+        return res.render('config/profiles.ejs')
+    },
+    homePage: (req, res) => {
+        return res.render('config/homePage.ejs')
     }
 }
--- src/routes/main.js
+++ src/routes/main.js
@@ -1,21 +1,15 @@
 const controllerMain = require('../controllers/main.js')
 const router = require('express').Router()
 
-const Database = require("../functions/queryDB.js");
-
 const use = fn => (req, res, next) => {
     Promise.resolve(fn(req, res, next)).catch(next)
 }
 
 router.get('/', use(controllerMain.index))
-
-router.get('/profiles', async (req, res) => { res.render('profiles.ejs') })
+router.get('/profile', use(controllerMain.profile))
 
 // config
-router.get('/config/insert', async (req, res) => { res.render('config/insert.ejs') })
-router.get('/config/profiles', async (req, res) => { res.render('config/profiles.ejs') })
-router.get('/config/homePage', async (req, res) => { res.render('config/homePage.ejs') })
-
-router.get('/profile', async (req, res) => { res.render('profile.ejs') })
+router.get('/config/profiles', use(controllerMain.profiles))
+router.get('/config/homePage', use(controllerMain.homePage))
 
 module.exports = router
